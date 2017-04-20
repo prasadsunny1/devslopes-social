73e7d54306e768d41a064b8ca1ad0327ca032fe4
sunny <prasadsunny1@gmail.com>
2017-04-20 20:29:25 +0530

    added facebook signIn with firebase
    facebook sdk added
    facebook signin flow with firebase indication works

--- devslopes social/AppDelegate.swift
+++ devslopes social/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,8 +19,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FIRApp.configure()
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        
+        return handled
+    }
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- devslopes social/SignInVC.swift
+++ devslopes social/SignInVC.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import Firebase
 import FirebaseDatabase
 import FirebaseAuth
+import FBSDKCoreKit
+import FBSDKLoginKit
+
 
 class SignInVC: UIViewController {
     var handle:FIRAuthStateDidChangeListenerHandle!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var a = Dictionary<String, Any>()
-        a = ["name":"sunny","surname":"prasad"]
-        let ref = FIRDatabase.database().reference()
-        ref.setValue(a)
-        ref.child("name").setValue(a)
+//        var a = Dictionary<String, Any>()
+//        a = ["name":"sunny","surname":"prasad"]
+//        let ref = FIRDatabase.database().reference()
+//        ref.setValue(a)
+//        ref.child("name").setValue(a)
     
     }
     override func viewWillAppear(_ animated: Bool) {
@@ -33,11 +37,37 @@ class SignInVC: UIViewController {
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func FacebookButton(_ sender: RoundBtn) {
 
+    let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, err) in
+            if err != nil{
+                print("unable to authenticate with facebook \(String(describing: err))")
+                
+                
+            }
+            else if result?.isCancelled == true{
+                print("user cancled the facebook login")
+                
+            }
+            else{
+                print("authenticated successfully with facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuthenticate(credential)
+            }
+        }
+    }
+    
+    
+    func firebaseAuthenticate(_ credential : FIRAuthCredential){
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, err) in
+            if err != nil {
+                print("unable to authenticate with Firebase \(String(describing: err))")
+            }else{
+                print("successfull authenticated with firebase")
+            }
+        })
+    }
 
 }
 
